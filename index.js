8fd5eefd408a1d631f2de7b09b82a43337eeba6d
AlexU <auteshev505@gmail.com>
2021-02-09 14:13:49 +0300

    add planned job check action add fast answers add NTBA_FIX_319

--- index.js
+++ index.js
@@ -1,44 +1,65 @@
+process.env.NTBA_FIX_319 = 1;
+
 require('dotenv').config({path: __dirname + '/.env'});
 const cron = require('node-cron');
 const TelegramBot = require('node-telegram-bot-api');
 
 const TOKEN = process.env.BOT_TOKEN;
 const CHAT_ID = process.env.CHAT_ID;
+const STATUS_JOB_SCHEDULE = '40 14 * * 1-5';
+const CALLS_JOB_SCHEDULE = '30 21 * * 1-5'
+const PORT = process.env.PORT
+const URL = process.env.APP_URL;
 
 const options = {
     polling: true,
-    port: process.env.PORT
+    port: PORT
 }
-const url = process.env.APP_URL;
 
 const bot = new TelegramBot(TOKEN, options);
 
-bot.setWebHook(`${url}/bot${TOKEN}`);
+bot.setWebHook(`${URL}/bot${TOKEN}`);
 
 bot.onText(/\/start/, async (msg) => {
     const chatId = msg.chat.id;
     await bot.sendMessage(CHAT_ID, `chat id ${chatId}`)
 });
 
+bot.onText(/\/jobs/, async (msg) => {
+    const chatId = msg.chat.id;
+
+    if (CHAT_ID) {
+        await bot.sendMessage(chatId, `Статус - ${STATUS_JOB_SCHEDULE}, вечерний созвон - ${CALLS_JOB_SCHEDULE}`);
+        return;
+    }
+
+    await bot.sendMessage(chatId, `Нет запланированных работ`);
+})
+
+
 if (CHAT_ID) {
     console.log('jobs set');
 
-    cron.schedule('18 13 * * 1-5', async () => {
-        console.log('send 18 13 * * 1-5');
+    cron.schedule(STATUS_JOB_SCHEDULE, async () => {
+        console.log(STATUS_JOB_SCHEDULE);
         await getStatusInfo();
     })
 
-    cron.schedule('30 21 * * 1-5', async () => {
-        console.log('send 30 21 * * 1-5');
+    cron.schedule(CALLS_JOB_SCHEDULE, async () => {
+        console.log(CALLS_JOB_SCHEDULE);
         await createPoll();
     })
 }
 
+// short answers
 bot.onText(/лол/, async (msg) => {
     const chatId = msg.chat.id;
     await bot.sendMessage(chatId, `кек`)
 })
-
+bot.onText(/\)\)\)\)/, async (msg) => {
+    const chatId = msg.chat.id;
+    await bot.sendMessage(chatId, `)))0)00`)
+})
 // cron events
 const getStatusInfo = async () => {
     const status_time = '15:00'
