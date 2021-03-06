969ed70b323886b6757fae0acd3d9a0dc904881b
AlexU <auteshev505@gmail.com>
2021-03-06 12:10:23 +0300

    add try catch block on job events

--- src/modules/jobs/JobEvents.ts
+++ src/modules/jobs/JobEvents.ts
@@ -7,46 +7,58 @@ export const getStatusInfo = async (opt: IJobMethodOptions) => {
     const {bot, chatId} = opt;
     const status_time = '15:00'
     const mention = '@kmsid'
-
-    await bot.sendMessage(chatId, `${mention} \nКирилл, будет статус в ${status_time}?`, {
-        reply_markup: {
-            keyboard: [
-                [{text: 'Да'}],
-                [{text: 'Нет'}]
-            ],
-            one_time_keyboard: true,
-            selective: true,
-        }
-    });
+    try {
+        await bot.sendMessage(chatId, `${mention} \nКирилл, будет статус в ${status_time}?`, {
+            reply_markup: {
+                keyboard: [
+                    [{text: 'Да'}],
+                    [{text: 'Нет'}]
+                ],
+                one_time_keyboard: true,
+                selective: true,
+            }
+        });
+    } catch (e) {
+        console.error('getStatusInfo', e)
+    }
 };
 
 export const createStatusPoll = async (opt: IJobMethodOptions) => {
-    const {bot, chatId} = opt;
-    const question = `Ребзя, нужен созвон с Андреем?`;
-    const answers = ['Да', 'Нет'];
-    const options = {
-        is_anonymous: false,
+    try {
+        const {bot, chatId} = opt;
+        const question = `Ребзя, нужен созвон с Андреем?`;
+        const answers = ['Да', 'Нет'];
+        const options = {
+            is_anonymous: false,
+        }
+        const msg = await bot.sendPoll(chatId, question, answers, options);
+        const poll: IPoll = {
+            chatId,
+            pollMessageId: msg.message_id,
+            question,
+            answers,
+            pollId: msg.poll!.id,
+        }
+        await PollController.createPoll(poll);
+        await bot.pinChatMessage(chatId, String(msg.message_id));
+
+    } catch (e) {
+        console.error('createStatusPoll', e)
     }
-    const msg = await bot.sendPoll(chatId, question, answers, options);
-    const poll: IPoll = {
-        chatId,
-        pollMessageId: msg.message_id,
-        question,
-        answers,
-        pollId: msg.poll!.id,
-    }
-    await PollController.createPoll(poll);
-    await bot.pinChatMessage(chatId, String(msg.message_id));
 };
 
 // TODO unpin poll not by cron job, but after N hour after createStatusPoll method calls
 export const unpinPoll = async (opt: IJobMethodOptions) => {
-    const {bot, chatId} = opt;
-    const poll = await PollController.getLastPoll(chatId);
-    if (!poll) {
-        return;
+    try {
+        const {bot, chatId} = opt;
+        const poll = await PollController.getLastPoll(chatId);
+        if (!poll) {
+            return;
+        }
+        await bot.unpinChatMessage(chatId);
+    } catch (e) {
+        console.error('unpinPoll', e)
     }
-    await bot.unpinChatMessage(chatId);
 }
 
 // export only for seeders
